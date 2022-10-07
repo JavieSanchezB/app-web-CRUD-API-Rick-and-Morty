914630c9bf83dceb2c48aab317611441cb5e9d6f
horusjavier <kornjaviersanchezb@gmail.com>
2022-10-07 00:02:16 -0500

    Configurando las rutas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('api')->group(function () {
+    Route::resource('usuarios', UsuariosController::class);
+    Route::resource('favoritos', FavoritostController::class);
 });
