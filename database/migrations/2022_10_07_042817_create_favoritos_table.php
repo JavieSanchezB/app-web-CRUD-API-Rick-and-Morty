53a45bdcdd79867d0ae8a998bf515b38c67ae807
horusjavier <kornjaviersanchezb@gmail.com>
2022-10-06 23:34:21 -0500

    creando modelo de datos con ORM

---
+++ database/migrations/2022_10_07_042817_create_favoritos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favoritos', function (Blueprint $table) {
+            $table->bigint('id_usuario');
+            $table->string('ref_api');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favoritos');
+    }
+}
